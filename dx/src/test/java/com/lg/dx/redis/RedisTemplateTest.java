06ce967e033a517c9c36cc9b37cf778d1bd293f8
shAhnRepositories <anjh723@naver.com>
2022-06-26 19:00:23 +0900

    feat : connect with local redis server

---
+++ dx/src/test/java/com/lg/dx/redis/RedisTemplateTest.java
@@ -0,0 +1,81 @@
+package com.lg.dx.redis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
+
+import com.lg.dx.app.DxApplication;
+
+@SpringBootTest(classes = DxApplication.class)
+@PropertySource("classpath:application.properties")
+public class RedisTemplateTest {
+	@Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    void testStrings() {
+        // given
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String key = "stringKey";
+
+        // when
+        valueOperations.set(key, "hello");
+
+        // then
+        String value = valueOperations.get(key);
+        assertEquals(value, "hello");
+    }
+
+
+    @Test
+    void testSet() {
+        // given
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        String key = "setKey";
+
+        // when
+        setOperations.add(key, "h", "e", "l", "l", "o");
+
+        // then
+        Set<String> members = setOperations.members(key);
+        Long size = setOperations.size(key);
+
+        assertTrue(members.contains("h"));
+        assertTrue(members.contains("e"));
+        assertTrue(members.contains("l"));
+        assertTrue(members.contains("o"));
+        assertEquals(size, 4);
+    }
+
+    @Test
+    void testHash() {
+        // given
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        String key = "hashKey";
+
+        // when
+        hashOperations.put(key, "hello", "world");
+
+        // then
+        Object value = hashOperations.get(key, "hello");
+        assertEquals("world", value);
+
+        Map<Object, Object> entries = hashOperations.entries(key);
+        assertTrue(entries.keySet().contains("hello"));
+        assertTrue(entries.values().contains("world"));
+
+        Long size = hashOperations.size(key);
+        assertEquals(entries.size(), size);
+    }
+}
